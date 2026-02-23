0f000a40667429cc966a570398353c9eb39f3322
Evan Hermenau <evan.hermenau@email.com>
2026-02-23 16:08:06 -0500

    kubernetes namespace, argocd helm release, argocd application

---
+++ provision/hub-eks/argocd.tf
@@ -0,0 +1,72 @@
+# 1. Dedicated Namespace with Interconnect Override
+resource "kubernetes_namespace" "argocd" {
+  metadata {
+    name = "argocd"
+
+    labels = {
+      Space = "Interconnect"
+    }
+  }
+}
+
+# 2. ArgoCD Helm Release
+resource "helm_release" "argocd" {
+  name       = "argocd"
+  repository = "https://argoproj.github.io/argo-helm"
+  chart      = "argo-cd"
+  namespace  = kubernetes_namespace.argocd.metadata[0].name
+  version    = "9.4.4"
+
+  # We use 'set' for high-level toggles
+  set {
+    name  = "server.extraArgs"
+    value = "{--insecure}" # TLS termination will happen at the AWS LB later
+  }
+
+  # Injects the 'Interconnect' tag into all resources created by the Helm chart
+  values = [
+    <<-EOF
+    global:
+      additionalLabels:
+        Space: Interconnect
+        Project: EFI
+    server:
+      service:
+        type: NodePort
+    EOF
+  ]
+
+  depends_on = [kubernetes_namespace.argocd]
+}
+
+# 3. The Root Application (The GitOps Anchor)
+resource "kubernetes_manifest" "root_app" {
+  manifest = {
+    apiVersion = "argoproj.io/v1alpha1"
+    kind       = "Application"
+    metadata = {
+      name      = "root-app"
+      namespace = "argocd"
+    }
+    spec = {
+      project = "default"
+      source = {
+        repoURL        = "https://gitlab.com/evanhermenau/edge-first-infrastructure.git"
+        targetRevision = "task/efi-bootstrap-argodc"
+        path           = "gitops/hub/root"
+      }
+      destination = {
+        server    = "https://kubernetes.default.svc"
+        namespace = "argocd"
+      }
+      syncPolicy = {
+        automated = {
+          prune    = true
+          selfHeal = true
+        }
+      }
+    }
+  }
+
+  depends_on = [helm_release.argocd]
+}
