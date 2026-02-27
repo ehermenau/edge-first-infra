b80e38ad593875bc34b430536368bbb8fdf14c21
Evan Hermenau <evan.hermenau@email.com>
2026-02-26 20:22:10 -0500

    remove interconnect label

--- provision/hub/dns.tf
+++ provision/hub/dns.tf
@@ -4,10 +4,4 @@ resource "aws_route53_zone" "internal" {
   vpc {
     vpc_id = module.vpc.vpc_id
   }
-
-  # This tag overrides the 'Blue' tag from providers.tf
-  tags = {
-    Name  = "efi-internal-phz"
-    Space = "Interconnect"
-  }
 }
