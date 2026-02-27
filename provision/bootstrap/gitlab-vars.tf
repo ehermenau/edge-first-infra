cee1fd5fdfb5fa114e47b68ec140b075a6f84725
Evan Hermenau <evan.hermenau@email.com>
2026-02-27 05:49:52 -0500

    push IAM role ARN and TF state bucket vars to gitlab

---
+++ provision/bootstrap/gitlab-vars.tf
@@ -0,0 +1,16 @@
+# 1. Automatically push the ARN back to GitLab
+resource "gitlab_project_variable" "aws_role_arn" {
+  project   = "evanhermenau/edge-first-infrastructure"
+  key       = "AWS_ROLE_ARN"
+  value     = aws_iam_role.gitlab_ci_role.arn
+  protected = true
+  masked    = true
+}
+
+# 2. Push the S3 Bucket name for the other tiers
+resource "gitlab_project_variable" "tf_state_bucket" {
+  project   = "evanhermenau/edge-first-infrastructure"
+  key       = "TF_STATE_BUCKET"
+  value     = aws_s3_bucket.state.bucket
+  protected = true
+}
