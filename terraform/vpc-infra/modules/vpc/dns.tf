aba5b57bab9abf3e1d73cb0e1cf4ca96845be60c
Evan Hermenau <evan.hermenau@email.com>
2026-03-01 10:24:38 -0500

    Name tag on route53 hosted zone

--- terraform/vpc-infra/modules/vpc/dns.tf
+++ terraform/vpc-infra/modules/vpc/dns.tf
@@ -1,7 +1,11 @@
 resource "aws_route53_zone" "internal" {
-  name = "efi.internal"
+  name = "${var.environment}.${var.namespace}"
 
   vpc {
     vpc_id = module.vpc.vpc_id
+  }
+
+  tags = {
+    Name = "${var.environment}-internal-zone"
   }
 }
