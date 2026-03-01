db73a7ab7d278a7410eb74b21f224a982b294f9d
Evan Hermenau <evan.hermenau@email.com>
2026-03-01 10:12:47 -0500

    add validation to environment variable

--- terraform/vpc-infra/modules/vpc/variables.tf
+++ terraform/vpc-infra/modules/vpc/variables.tf
@@ -14,4 +14,8 @@ variable "vpc_cidr" {
 variable "environment" {
   description = "Environment name"
   type        = string
+  validation {
+    condition     = contains(["staging", "prod"], var.environment)
+    error_message = "Environment must be staging or prod."
+  }
 }
