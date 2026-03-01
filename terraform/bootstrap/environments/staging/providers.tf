a832165b67048e24f672a8db092e51ef7ca9d524
Evan Hermenau <evan.hermenau@email.com>
2026-03-01 13:57:52 -0500

    update backend s3 with remote backend

--- terraform/bootstrap/environments/staging/providers.tf
+++ terraform/bootstrap/environments/staging/providers.tf
@@ -12,12 +12,12 @@ terraform {
     }
   }
 
-  # backend "s3" {
-  # bucket       = "" # Passed in via var 
-  # key          = "staging/bootstrap/terraform.tfstate"
-  # region       = "us-east-1"
-  # use_lockfile = true
-  # }
+  backend "s3" {
+    bucket       = "efi-staging-terraform-state"
+    key          = "staging/bootstrap/terraform.tfstate"
+    region       = "us-east-1"
+    use_lockfile = true
+  }
 }
 
 provider "aws" {
