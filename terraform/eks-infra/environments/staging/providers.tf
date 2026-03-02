4aacf362f0c41ad5038cb17ea3b2e53427c59bf1
Evan Hermenau <evan.hermenau@email.com>
2026-03-02 06:07:07 -0500

    remove helm and kubernetes providers from eks-infra

--- terraform/eks-infra/environments/staging/providers.tf
+++ terraform/eks-infra/environments/staging/providers.tf
@@ -10,14 +10,6 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 6.0"
     }
-    kubernetes = {
-      source  = "hashicorp/kubernetes"
-      version = "3.0.1"
-    }
-    helm = {
-      source  = "hashicorp/helm"
-      version = "3.1.1"
-    }
   }
 
   backend "s3" {
