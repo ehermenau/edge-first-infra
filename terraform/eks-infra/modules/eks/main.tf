b86ef811356ca6005ca9fff95996d348baa412a9
Evan Hermenau <evan.hermenau@email.com>
2026-03-02 06:23:04 -0500

    remove argoCD from eks-infra, remove keys where we're using default values

--- terraform/eks-infra/modules/eks/main.tf
+++ terraform/eks-infra/modules/eks/main.tf
@@ -2,19 +2,17 @@ module "eks" {
   source  = "terraform-aws-modules/eks/aws"
   version = "~> 21.0"
 
-  name               = "efi-hub-cluster"
+  name               = "efi-${var.environment}-eks-cluster"
   kubernetes_version = "1.35"
 
   vpc_id     = data.aws_vpc.hub.id
   subnet_ids = data.aws_subnets.private.ids
 
-  # Security: The Hybrid access model
-  endpoint_public_access  = true
-  endpoint_private_access = true
-  # endpoint_public_access_cidrs = ["${chomp(data.http.workstation_external_ip.response_body)}/32"]
+  # Enable public access until we can lock it down via ZT 
+  endpoint_public_access = true
 
   # Identity: Add current user as admin
-  # enable_cluster_creator_admin_permissions = true
+  enable_cluster_creator_admin_permissions = true
 
   # EKS Auto Mode: AWS manages the 'general-purpose' compute nodes
   compute_config = {
