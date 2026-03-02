0e4b472192ae2b1db20e3f6fde3db56d194c9ae9
Evan Hermenau <evan.hermenau@email.com>
2026-03-02 07:08:36 -0500

    eks module variables, environment|aws_role_arn|admin_user_arn

--- terraform/eks-infra/modules/eks/variables.tf
+++ terraform/eks-infra/modules/eks/variables.tf
@@ -6,3 +6,13 @@ variable "environment" {
     error_message = "Environment must be staging or prod."
   }
 }
+
+variable "aws_role_arn" {
+  description = "ARN of the IAM Role used by gitlab CICD"
+  type        = string
+}
+
+variable "admin_user_arn" {
+  description = "ARN of the admin user that will access the EKS cluster"
+  type        = string
+}
