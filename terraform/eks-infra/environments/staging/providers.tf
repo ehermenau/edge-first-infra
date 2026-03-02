a2abb147bc3b8a97b9b80342b58f41abdf73b476
Evan Hermenau <evan.hermenau@email.com>
2026-03-02 05:16:17 -0500

    update eks providers.tf

--- terraform/eks-infra/environments/staging/providers.tf
+++ terraform/eks-infra/environments/staging/providers.tf
@@ -22,7 +22,7 @@ terraform {
 
   backend "s3" {
     bucket       = "" # Passed in via var 
-    key          = "hub-eks/terraform.tfstate"
+    key          = "staging/eks/terraform.tfstate"
     region       = "us-east-1"
     use_lockfile = true
   }
@@ -32,9 +32,10 @@ provider "aws" {
   region = "us-east-1"
   default_tags {
     tags = {
-      Project   = "Edge-First-Infrastructure"
-      ManagedBy = "Terraform"
-      Component = "Control-Plane"
+      Project     = "Edge-First-Infrastructure"
+      ManagedBy   = "Terraform"
+      Component   = "Control-Plane"
+      Environment = "Staging"
     }
   }
 }
@@ -44,7 +45,7 @@ provider "kubernetes" {
   cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
 
   exec {
-    api_version = "client.authentication.k8s.io/v1" # Standard for 2026
+    api_version = "client.authentication.k8s.io/v1"
     command     = "aws"
     args        = ["eks", "get-token", "--cluster-name", module.eks.cluster_name]
   }
