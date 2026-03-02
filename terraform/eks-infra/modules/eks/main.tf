8f8490e15f2925f7691ab54db0799e2dcfa7b568
Evan Hermenau <evan.hermenau@email.com>
2026-03-02 06:12:16 -0500

    remove SG creation for 10250

--- terraform/eks-infra/modules/eks/main.tf
+++ terraform/eks-infra/modules/eks/main.tf
@@ -9,28 +9,16 @@ module "eks" {
   subnet_ids = data.aws_subnets.private.ids
 
   # Security: The Hybrid access model
-  endpoint_public_access       = true
-  endpoint_private_access      = true
-  endpoint_public_access_cidrs = ["${chomp(data.http.workstation_external_ip.response_body)}/32"]
+  endpoint_public_access  = true
+  endpoint_private_access = true
+  # endpoint_public_access_cidrs = ["${chomp(data.http.workstation_external_ip.response_body)}/32"]
 
   # Identity: Add current user as admin
-  enable_cluster_creator_admin_permissions = true
+  # enable_cluster_creator_admin_permissions = true
 
   # EKS Auto Mode: AWS manages the 'general-purpose' compute nodes
   compute_config = {
     enabled    = true
     node_pools = ["general-purpose"]
   }
-}
-
-resource "aws_security_group_rule" "control_plane_to_node_kubelet" {
-  description = "Allow EKS Control Plane to reach Kubelet for logs/exec"
-  type        = "ingress"
-  from_port   = 10250
-  to_port     = 10250
-  protocol    = "tcp"
-
-  # Reference the module outputs directly
-  security_group_id        = module.eks.node_security_group_id
-  source_security_group_id = module.eks.cluster_primary_security_group_id
 }
