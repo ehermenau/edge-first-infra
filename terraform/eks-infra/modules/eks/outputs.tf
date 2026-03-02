0177fec967f61eeba9f7be6be55ddc7b596e0e29
Evan Hermenau <evan.hermenau@email.com>
2026-03-02 06:23:58 -0500

    add cluster name and OIDC ARN outputs to eks-infra

--- terraform/eks-infra/modules/eks/outputs.tf
+++ terraform/eks-infra/modules/eks/outputs.tf
@@ -6,3 +6,11 @@ output "cluster_endpoint" {
 output "cluster_certificate_authority_data" {
   value = module.eks.cluster_certificate_authority_data
 }
+
+output "oidc_provider_arn" {
+  value = module.eks.oidc_provider_arn
+}
+
+output "cluster_name" {
+  value = module.eks.cluster_name
+}
