243eaa8ad2b3bf40a520812fe605871c2575a4e4
Evan Hermenau <evan.hermenau@email.com>
2026-03-02 06:15:09 -0500

    output EKS endpoint & CA data

--- terraform/eks-infra/modules/eks/outputs.tf
+++ terraform/eks-infra/modules/eks/outputs.tf
@@ -1,9 +1,8 @@
-output "configure_kubectl" {
-  description = "Command to update your kubeconfig"
-  value       = "aws eks update-kubeconfig --region us-east-1 --name ${module.eks.cluster_name}"
-}
-
 output "cluster_endpoint" {
   description = "The endpoint for your EKS Kubernetes API."
   value       = module.eks.cluster_endpoint
+}
+
+output "cluster_certificate_authority_data" {
+  value = module.eks.cluster_certificate_authority_data
 }
