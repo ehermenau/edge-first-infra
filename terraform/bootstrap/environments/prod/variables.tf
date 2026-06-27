282cc53c1ade28979c1ea12f7002b1e7fc50af97
Evan Hermenau <evan@fetchlabs.io>
2026-06-27 16:46:27 -0400

    add protected_branches and repository variables for git module

--- terraform/bootstrap/environments/prod/variables.tf
+++ terraform/bootstrap/environments/prod/variables.tf
@@ -2,3 +2,13 @@ variable "environment" {
   type    = string
   default = "prod"
 }
+
+variable "protected_branches" {
+  type    = bool
+  default = true
+}
+
+variable "repository" {
+  type    = string
+  default = "ehermenau/edge-first-infra"
+}
--- terraform/bootstrap/environments/staging/variables.tf
+++ terraform/bootstrap/environments/staging/variables.tf
@@ -2,3 +2,13 @@ variable "environment" {
   type    = string
   default = "staging"
 }
+
+variable "protected_branches" {
+  type    = bool
+  default = false
+}
+
+variable "repository" {
+  type    = string
+  default = "ehermenau/edge-first-infra"
+}
