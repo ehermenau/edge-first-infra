3deae5a2792b371a9fbea83f8c4080dde00f6a2b
Evan Hermenau <evan@fetchlabs.io>
2026-06-27 16:47:58 -0400

    add execution of git_vars module to prod and staging environments

--- terraform/bootstrap/environments/staging/main.tf
+++ terraform/bootstrap/environments/staging/main.tf
@@ -7,3 +7,10 @@ module "oidc_iam" {
   source      = "../../modules/oidc_iam"
   environment = var.environment
 }
+
+module "git_vars" {
+  source             = "../../modules/git_vars"
+  environment        = var.environment
+  repository         = var.repository
+  protected_branches = var.protected_branches
+}
